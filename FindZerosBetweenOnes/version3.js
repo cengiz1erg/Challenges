5f088921676c99e5848917e64823b4a2b778b64a
Cengiz Ergün <cengiz1ergun@gmail.com>
2022-03-20 11:54:50 +0300

    version3 with regex

---
+++ FindZerosBetweenOnes/version3.js
@@ -0,0 +1,22 @@
+console.log("version 3 started")
+
+function solution(Number){
+    try {
+        var binaryResult = (Number >>> 0).toString(2);
+        var reg = /10+1/g;
+        var matches = [], found;
+        while (found = reg.exec(binaryResult)) {
+            matches.push(found[0]);
+            reg.lastIndex = found[0].length + found.index - 1
+        }        
+        if (matches.length == 0) {
+            return 0
+        }else{
+            return Math.max(...(matches.map(match => match.length - 2)))
+        }
+    } catch (error) {
+        console.log("err: " + err);
+    }
+}
+
+console.log(solution("10000010001"))
